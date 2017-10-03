8e23e3c9872b7664ca98927ae969037b3c784b5b
Ha Nhat Lenh <lenh@ktc.local>
2017-10-03 20:06:48 +0900

    Commited from lenh at Tue, Oct 03, 2017  8:06:47 PM

--- modules/users/server/controllers/users/users.profile.server.controller.js
+++ modules/users/server/controllers/users/users.profile.server.controller.js
@@ -96,6 +96,65 @@ exports.changeProfilePicture = function (req, res) {
 };
 
 /**
+ * Change Password
+ */
+exports.changePassword = function (req, res, next) {
+  // Init Variables
+  var passwordDetails = req.body;
+
+  if (req.user) {
+    if (passwordDetails.newPassword) {
+      User.findById(req.user.id, function (err, user) {
+        if (!err && user) {
+          if (user.authenticate(passwordDetails.currentPassword)) {
+            if (passwordDetails.newPassword === passwordDetails.verifyPassword) {
+              user.password = passwordDetails.newPassword;
+
+              user.save(function (err) {
+                if (err) {
+                  return res.status(422).send({
+                    message: errorHandler.getErrorMessage(err)
+                  });
+                } else {
+                  req.login(user, function (err) {
+                    if (err) {
+                      res.status(400).send(err);
+                    } else {
+                      res.send({
+                        message: 'Password changed successfully'
+                      });
+                    }
+                  });
+                }
+              });
+            } else {
+              res.status(422).send({
+                message: 'Passwords do not match'
+              });
+            }
+          } else {
+            res.status(422).send({
+              message: 'Current password is incorrect'
+            });
+          }
+        } else {
+          res.status(400).send({
+            message: 'User is not found'
+          });
+        }
+      });
+    } else {
+      res.status(422).send({
+        message: 'Please provide a new password'
+      });
+    }
+  } else {
+    res.status(401).send({
+      message: 'User is not signed in'
+    });
+  }
+};
+/**
  * Send User
  */
 exports.me = function (req, res) {
