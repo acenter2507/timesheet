1e5bf80746e5a90b3b20785e06bb51065dc7ad96
Ha Nhat Lenh <lenh@ktc.local>
2017-10-04 20:46:09 +0900

    Commited from lenh at Wed, Oct 04, 2017  8:46:09 PM

--- modules/departments/client/config/departments.client.config.js
+++ modules/departments/client/config/departments.client.config.js
@@ -7,7 +7,7 @@
 
   menuConfig.$inject = ['Menus'];
 
-  function menuConfig(menuService) {
+  function menuConfig(Menus) {
     // Set top bar menu items
     Menus.addMenuItem('topbar', {
       title: 'Departments',
