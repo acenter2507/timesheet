fd00299c1f92bab95ba1bb18b82d04e791f6626f
Ha Nhat Lenh <lenh@ktc.local>
2017-10-11 16:53:07 +0900

    Commited from lenh at Wed, Oct 11, 2017  4:53:07 PM

--- modules/departments/server/models/department.server.model.js
+++ modules/departments/server/models/department.server.model.js
@@ -31,11 +31,12 @@ DepartmentSchema.plugin(paginate);
 
 DepartmentSchema.statics.addLeader = function (departmentId, userId) {
   return this.findById(departmentId).exec(function (err, department) {
-    if (err || !department) return;
-    if (!_.contains(department.leaders, userId)) {
-      department.leaders.push(userId);
-      return department.save();
-    }
+    console.log(department);
+    // if (err || !department) return;
+    // if (!_.contains(department.leaders, userId)) {
+    //   department.leaders.push(userId);
+    //   return department.save();
+    // }
     return;
   });
 };
@@ -52,10 +53,14 @@ DepartmentSchema.statics.addMember = function (departmentId, userId) {
 };
 
 DepartmentSchema.statics.removeLeader = function (departmentId, userId) {
-  return this.findByIdAndUpdate(departmentId, {
-    '$pull': {
-      'leaders': { '_id': new Schema.ObjectId(userId) }
-    }
+  return this.findById(departmentId).exec(function (err, department) {
+    console.log(department);
+    // if (err || !department) return;
+    // if (_.contains(department.leaders, userId)) {
+    //   department.leaders.pull(userId);
+    //   return department.save();
+    // }
+    return;
   });
 };
 
