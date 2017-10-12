b5db15f59cf5363804c11f6b2de86f8818e321b2
Ha Nhat Lenh <lenh@ktc.local>
2017-10-12 17:57:36 +0900

    Commited from lenh at Thu, Oct 12, 2017  5:57:36 PM

--- modules/users/client/controllers/admin/user.client.controller.js
+++ modules/users/client/controllers/admin/user.client.controller.js
@@ -1,40 +1,61 @@
 'use strict';
 
-angular.module('users.admin').controller('UserController', ['$scope', '$state', 'Authentication', 'userResolve',
-  function ($scope, $state, Authentication, userResolve) {
-    $scope.authentication = Authentication;
-    $scope.user = userResolve;
+angular.module('users.admin').controller('UserController', ['$scope', '$state', 'userResolve',
+  function ($scope, $state, userResolve) {
+    var vm = this;
+    vm.user = userResolve;
 
-    $scope.remove = function (user) {
-      if (confirm('Are you sure you want to delete this user?')) {
-        if (user) {
-          user.$remove();
+    onCreate();
 
-          $scope.users.splice($scope.users.indexOf(user), 1);
-        } else {
-          $scope.user.$remove(function () {
-            $state.go('admin.users');
-          });
-        }
+    function onCreate() {
+      prepareSecurityCheck();
+    }
+
+    function prepareSecurityCheck() {
+      if (!$scope.isAdmin && !$scope.isAccountant) {
+        handlePreviousScreen();
+        return $scope.handleShowToast('このページを表示する権限がありません。', true);
       }
-    };
-
-    $scope.update = function (isValid) {
-      if (!isValid) {
-        $scope.$broadcast('show-errors-check-validity', 'userForm');
-
-        return false;
+      if (vm.user.status === 3 && !$scope.isAdmin) {
+        handlePreviousScreen();
+        return $scope.handleShowToast('このユーザーが削除されました。', true);
       }
+    }
+    // Trở về màn hình trước
+    function handlePreviousScreen() {
+      $state.go($state.previous.state.name || 'home', $state.previous.params);
+    }
 
-      var user = $scope.user;
+    // $scope.remove = function (user) {
+    //   if (confirm('Are you sure you want to delete this user?')) {
+    //     if (user) {
+    //       user.$remove();
 
-      user.$update(function () {
-        $state.go('admin.user', {
-          userId: user._id
-        });
-      }, function (errorResponse) {
-        $scope.error = errorResponse.data.message;
-      });
-    };
+    //       $scope.users.splice($scope.users.indexOf(user), 1);
+    //     } else {
+    //       $scope.user.$remove(function () {
+    //         $state.go('admin.users');
+    //       });
+    //     }
+    //   }
+    // };
+
+    // $scope.update = function (isValid) {
+    //   if (!isValid) {
+    //     $scope.$broadcast('show-errors-check-validity', 'userForm');
+
+    //     return false;
+    //   }
+
+    //   var user = $scope.user;
+
+    //   user.$update(function () {
+    //     $state.go('admin.user', {
+    //       userId: user._id
+    //     });
+    //   }, function (errorResponse) {
+    //     $scope.error = errorResponse.data.message;
+    //   });
+    // };
   }
 ]);
