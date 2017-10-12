b2d7a3c81a50f8b7184ada1ea596b6353ca8b43f
Ha Nhat Lenh <lenh@ktc.local>
2017-10-12 19:11:18 +0900

    Commited from lenh at Thu, Oct 12, 2017  7:11:18 PM

--- modules/users/client/filters/users-role.client.filter.js
+++ modules/users/client/filters/users-role.client.filter.js
@@ -13,7 +13,7 @@
       if (_.contains(roles, 'admin')) return 'システム管理';
       if (_.contains(roles, 'accountant')) return '経理部';
       if (_.contains(roles, 'manager')) return 'マネージャ';
-      return '社員';
+      return '一般社員';
     };
   }
 }());
