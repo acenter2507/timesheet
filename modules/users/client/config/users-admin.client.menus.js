7f66b82b815508859926cff73ef3ddfe9b697f7d
Ha Nhat Lenh <lenh@ktc.local>
2017-10-23 09:44:54 +0900

    Commited from lenh at Mon, Oct 23, 2017  9:44:54 AM

--- modules/users/client/config/users-admin.client.menus.js
+++ modules/users/client/config/users-admin.client.menus.js
@@ -7,7 +7,7 @@ angular.module('users.admin').run(['Menus',
       title: '社員',
       state: 'users.list',
       // type: 'dropdown',
-      roles: '*'
+      roles: ['user']
     });
 
     // Menus.addSubMenuItem('topbar', 'users', {
