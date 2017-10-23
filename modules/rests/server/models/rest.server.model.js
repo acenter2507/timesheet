7458f38ad485464693cec5e581122d646ba473d6
Ha Nhat Lenh <lenh@ktc.local>
2017-10-23 19:52:12 +0900

    Commited from lenh at Mon, Oct 23, 2017  7:52:12 PM

--- modules/rests/server/models/rest.server.model.js
+++ modules/rests/server/models/rest.server.model.js
@@ -13,8 +13,13 @@ var mongoose = require('mongoose'),
 var RestSchema = new Schema({
   // Kind of Rest: 1:有給休暇 / 2:午前半休　/ 3:午後半休 / 4:振替休暇 / 5:特別休暇 / 6:看護休暇 / 7:介護休暇 / 8:生理休暇
   kind: { type: Number, default: 0, required: true },
-  name: { type: String, default: '', required: 'Please fill Rest name', trim: true },
-
+  start: { type: Date },
+  end: { type: Date },
+  duration: { type: Number },
+  description: { type: String },
+  status: { type: Number },
+  comment: { type: String },
+  approved: { type: Schema.ObjectId, ref: 'User' },
   created: { type: Date, default: Date.now },
   user: { type: Schema.ObjectId, ref: 'User' }
 });
