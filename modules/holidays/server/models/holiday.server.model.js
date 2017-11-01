56894e2573cf36c1585474f9f98a4f9645c81380
Ha Nhat Lenh <lenh@ktc.local>
2017-11-01 15:50:25 +0900

    Commited from lenh at Wed, Nov 01, 2017  3:50:24 PM

--- modules/holidays/server/models/holiday.server.model.js
+++ modules/holidays/server/models/holiday.server.model.js
@@ -4,26 +4,18 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
+  paginate = require('mongoose-paginate'),
   Schema = mongoose.Schema;
 
 /**
  * Holiday Schema
  */
 var HolidaySchema = new Schema({
-  name: {
-    type: String,
-    default: '',
-    required: 'Please fill Holiday name',
-    trim: true
-  },
-  created: {
-    type: Date,
-    default: Date.now
-  },
-  user: {
-    type: Schema.ObjectId,
-    ref: 'User'
-  }
+  name: { type: String, default: '', required: true, trim: true },
+  isPaid: { type: Boolean, default: false },
+  created: { type: Date, default: Date.now },
+  user: { type: Schema.ObjectId, ref: 'User' }
 });
+HolidaySchema.plugin(paginate);
 
 mongoose.model('Holiday', HolidaySchema);
--- modules/holidays/server/policies/holidays.server.policy.js
+++ modules/holidays/server/policies/holidays.server.policy.js
@@ -11,62 +11,13 @@ acl = new acl(new acl.memoryBackend());
 /**
  * Invoke Holidays Permissions
  */
-exports.invokeRolesPolicies = function () {
-  acl.allow([{
-    roles: ['admin'],
-    allows: [{
-      resources: '/api/holidays',
-      permissions: '*'
-    }, {
-      resources: '/api/holidays/:holidayId',
-      permissions: '*'
-    }]
-  }, {
-    roles: ['user'],
-    allows: [{
-      resources: '/api/holidays',
-      permissions: ['get', 'post']
-    }, {
-      resources: '/api/holidays/:holidayId',
-      permissions: ['get']
-    }]
-  }, {
-    roles: ['guest'],
-    allows: [{
-      resources: '/api/holidays',
-      permissions: ['get']
-    }, {
-      resources: '/api/holidays/:holidayId',
-      permissions: ['get']
-    }]
-  }]);
-};
+exports.invokeRolesPolicies = function () { };
 
 /**
  * Check If Holidays Policy Allows
  */
 exports.isAllowed = function (req, res, next) {
-  var roles = (req.user) ? req.user.roles : ['guest'];
-
-  // If an Holiday is being processed and the current user created it then allow any manipulation
-  if (req.holiday && req.user && req.holiday.user && req.holiday.user.id === req.user.id) {
-    return next();
-  }
-
-  // Check for user roles
-  acl.areAnyRolesAllowed(roles, req.route.path, req.method.toLowerCase(), function (err, isAllowed) {
-    if (err) {
-      // An authorization error occurred
-      return res.status(500).send('Unexpected authorization error');
-    } else {
-      if (isAllowed) {
-        // Access granted! Invoke next middleware
-        return next();
-      } else {
-        return res.status(403).json({
-          message: 'User is not authorized'
-        });
-      }
-    }
-  });
+  var roles = (req.user) ? req.user.roles : [];
+  if (roles.indexOf('admin') >= 0 || roles.indexOf('accountant') >= 0) return next();
+  return res.status(403).json({ message: '権限がありません。' });
 };
