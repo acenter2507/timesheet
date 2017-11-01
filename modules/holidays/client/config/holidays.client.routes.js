9378eba475609b6c5bef8bf11e15e0c0d1f4f0fa
Ha Nhat Lenh <lenh@ktc.local>
2017-11-01 16:22:07 +0900

    Commited from lenh at Wed, Nov 01, 2017  4:22:07 PM

--- modules/holidays/client/config/holidays.client.routes.js
+++ modules/holidays/client/config/holidays.client.routes.js
@@ -12,54 +12,40 @@
       .state('holidays', {
         abstract: true,
         url: '/holidays',
-        template: '<ui-view/>'
+        template: '<ui-view/>',
+        data: { roles: ['admin', 'accountant'] },
+        ncyBreadcrumb: { label: '休日形態' }
       })
       .state('holidays.list', {
         url: '',
         templateUrl: 'modules/holidays/client/views/list-holidays.client.view.html',
         controller: 'HolidaysListController',
         controllerAs: 'vm',
-        data: {
-          pageTitle: 'Holidays List'
-        }
+        ncyBreadcrumb: { label: '一覧' }
       })
       .state('holidays.create', {
         url: '/create',
         templateUrl: 'modules/holidays/client/views/form-holiday.client.view.html',
         controller: 'HolidaysController',
         controllerAs: 'vm',
-        resolve: {
-          holidayResolve: newHoliday
-        },
-        data: {
-          roles: ['user', 'admin'],
-          pageTitle: 'Holidays Create'
-        }
+        resolve: { holidayResolve: newHoliday },
+        ncyBreadcrumb: { label: '追加' }
       })
       .state('holidays.edit', {
         url: '/:holidayId/edit',
         templateUrl: 'modules/holidays/client/views/form-holiday.client.view.html',
         controller: 'HolidaysController',
         controllerAs: 'vm',
-        resolve: {
-          holidayResolve: getHoliday
-        },
-        data: {
-          roles: ['user', 'admin'],
-          pageTitle: 'Edit Holiday {{ holidayResolve.name }}'
-        }
+        resolve: { holidayResolve: getHoliday },
+        ncyBreadcrumb: { label: '編集' }
       })
       .state('holidays.view', {
         url: '/:holidayId',
         templateUrl: 'modules/holidays/client/views/view-holiday.client.view.html',
         controller: 'HolidaysController',
         controllerAs: 'vm',
-        resolve: {
-          holidayResolve: getHoliday
-        },
-        data: {
-          pageTitle: 'Holiday {{ holidayResolve.name }}'
-        }
+        resolve: { holidayResolve: getHoliday },
+        ncyBreadcrumb: { label: '詳細' }
       });
   }
 
--- modules/holidays/client/controllers/list-holidays.client.controller.js
+++ modules/holidays/client/controllers/list-holidays.client.controller.js
@@ -5,11 +5,67 @@
     .module('holidays')
     .controller('HolidaysListController', HolidaysListController);
 
-  HolidaysListController.$inject = ['HolidaysService'];
+  HolidaysListController.$inject = [
+    '$scope',
+    'HolidaysService',
+    'ngDialog'
+  ];
 
-  function HolidaysListController(HolidaysService) {
+  function HolidaysListController($scope, HolidaysService, ngDialog) {
     var vm = this;
-
     vm.holidays = HolidaysService.query();
+
+    vm.handleAddNewHoliday = () => {
+      $scope.holiday = new HolidaysService();
+      $scope.handleSaveHoliday = handleSaveHoliday;
+
+      // Open dialog
+      var mDialog = ngDialog.open({
+        templateUrl: 'formHoliday.html',
+        scope: $scope
+      });
+      mDialog.closePromise.then(function (res) {
+        if (_.isUndefined(res.value) ) return;
+      });
+    }
+    vm.handleEditHoliday = holiday => {
+      $scope.holiday = holiday;
+      $scope.handleSaveHoliday = handleSaveHoliday;
+
+      // Open dialog
+      var mDialog = ngDialog.open({
+        templateUrl: 'formHoliday.html',
+        scope: $scope
+      });
+      mDialog.closePromise.then(function (res) {
+        if (_.isUndefined(res.value) ) return;
+      });
+    };
+
+    // Save Holiday
+    function handleSaveHoliday(isValid) {
+      if (!isValid) {
+        $scope.$broadcast('show-errors-check-validity', 'holidayForm');
+        return false;
+      }
+
+      // TODO: move create/update logic to service
+      if ($scope.holiday._id) {
+        $scope.holiday.$update(successCallback, errorCallback);
+      } else {
+        $scope.holiday.$save(successCallback, errorCallback);
+      }
+
+      function successCallback(res) {
+        vm.holidays.push(res);
+        $scope.handleShowToast('休日形態を保存しました', false);
+        delete $scope.holiday;
+      }
+
+      function errorCallback(res) {
+        $scope.handleShowToast('休日形態を保存できません', true);
+        delete $scope.holiday;
+      }
+    }
   }
 }());
