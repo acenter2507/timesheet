6afadbcc044f111913a403d0a62ef076ca1da284
Ha Nhat Lenh <lenh@ktc.local>
2017-11-14 09:47:32 +0900

    Commited from lenh at Tue, Nov 14, 2017  9:47:32 AM

--- modules/core/client/services/date-util.client.service.js
+++ modules/core/client/services/date-util.client.service.js
@@ -9,17 +9,17 @@ function DateUtil() {
     if (duration < 0) {
       return duration;
     }
-    if (duration === 0 && (start.getDay() === 0 || start.getDay() === 6)) {
+    if (duration === 0 && (start.day() === 0 || start.day() === 6)) {
       return 0;
     }
-    if (duration === 0 && start.getDay() !== 0 && start.getDay() !== 6) {
+    if (duration === 0 && start.day() !== 0 && start.day() !== 6) {
       return 1;
     }
     var cnt = 1;
     var temp;
     for (let index = 1; index <= duration; index++) {
       temp = start.clone().add(index, 'days');
-      if (temp.getDay() !== 0 && temp.getDay() !== 6) {
+      if (temp.day() !== 0 && temp.day() !== 6) {
         cnt += 1;
       }
     }
