358a7461ecce32b5af15125ed3abce4af5df4caf
Ha Nhat Lenh <lenh@ktc.local>
2017-11-14 19:12:40 +0900

    Commited from lenh at Tue, Nov 14, 2017  7:12:40 PM

--- modules/rests/client/controllers/input-rests.client.controller.js
+++ modules/rests/client/controllers/input-rests.client.controller.js
@@ -92,6 +92,8 @@
       function successCallback(res) {
         vm.busy = false;
         console.log(res);
+        var a = moment(res.start);
+        console.log(a.format());
         // $state.go('rests.view', {
         //   restId: res._id
         // });
