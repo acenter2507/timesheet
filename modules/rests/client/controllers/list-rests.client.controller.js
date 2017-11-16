08bbc8adfce7079f182b38c36fae1a10c4e2332b
Ha Nhat Lenh <lenh@ktc.local>
2017-11-16 11:16:19 +0900

    Commited from lenh at Thu, Nov 16, 2017 11:16:18 AM

--- modules/rests/client/controllers/list-rests.client.controller.js
+++ modules/rests/client/controllers/list-rests.client.controller.js
@@ -5,11 +5,53 @@
     .module('rests')
     .controller('RestsListController', RestsListController);
 
-  RestsListController.$inject = ['RestsService'];
+  RestsListController.$inject = ['RestsService', 'CommonService', 'DateUtil'];
 
-  function RestsListController(RestsService) {
+  function RestsListController(RestsService, CommonService, DateUtil) {
     var vm = this;
-
     vm.rests = RestsService.query();
+
+    onCreate();
+    function onCreate() {
+      prepareCalendar();
+    }
+    function prepareCalendar() {
+      vm.calendar = { view: 'month' };
+      vm.calendar.viewDate = moment(vm.rest.start).startOf('month').toDate();
+      vm.calendar.cellModifier = function (cell) {
+        // cell.cssClass = 'odd-cell';
+        var date = cell.date.format('YYYY/MM/DD');
+
+        // 週末チェック
+        if (DateUtil.isWeekend(cell.date)) {
+          return;
+        }
+
+        // 祝日チェック
+        if (DateUtil.isWeekend(cell.date)) {
+          return;
+        }
+        var offdate = JapaneseHolidays.isHoliday(new Date(date));
+        if (offdate) {
+          cell.cssClass = 'off-cell';
+          return;
+        }
+
+        // 選択された範囲チェック
+        if (cell.date.isBetween(vm.rest.start, vm.rest.end, null, '[]')) {
+          cell.cssClass = 'selected-cell';
+          return;
+        }
+      };
+    }
+    vm.handleCalendarEventClicked = () => {
+      return false;
+    };
+    vm.handleCalendarRangeSelected = (start, end) => {
+      return false;
+    };
+    vm.handleCalendarClicked = date => {
+      return false;
+    };
   }
 }());
