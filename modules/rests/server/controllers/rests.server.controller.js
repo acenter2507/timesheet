2950eecaca41acd48af9a81794e4eabea0f50908
Ha Nhat Lenh <lenh@ktc.local>
2017-11-24 15:53:39 +0900

    Commited from lenh at Fri, Nov 24, 2017  3:53:39 PM

--- modules/rests/server/controllers/rests.server.controller.js
+++ modules/rests/server/controllers/rests.server.controller.js
@@ -11,7 +11,7 @@ var path = require('path'),
   _ = require('underscore'),
   m = require('moment');
   // m.tz.setDefault("Asia/Tokyo");
-  console.log(m().format());
+  console.log(new m().format());
 
 /**
  * Create a Rest
