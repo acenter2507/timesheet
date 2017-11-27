df751697d5c2ecedea30533103112473ecc958c3
Ha Nhat Lenh <lenh@ktc.local>
2017-11-27 14:20:39 +0900

    Commited from lenh at Mon, Nov 27, 2017  2:20:39 PM

--- modules/notifications/server/models/notification.server.model.js
+++ modules/notifications/server/models/notification.server.model.js
@@ -4,26 +4,21 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
+  paginate = require('mongoose-paginate'),
   Schema = mongoose.Schema;
 
 /**
  * Notification Schema
  */
 var NotificationSchema = new Schema({
-  name: {
-    type: String,
-    default: '',
-    required: 'Please fill Notification name',
-    trim: true
-  },
-  created: {
-    type: Date,
-    default: Date.now
-  },
-  user: {
-    type: Schema.ObjectId,
-    ref: 'User'
-  }
+  from: { type: Schema.ObjectId, ref: 'User' },
+  to: { type: Schema.ObjectId, ref: 'User' },
+  // 0: like poll, 1: dislike, 2: reply, 3: comment, 4: option request
+  type: { type: Number, default: 0 },
+  message: { type: String, required: true },
+  state: { type: String, default: '' },
+  status: { type: Number, default: 0 },
+  created: { type: Date, default: Date.now }
 });
 
 mongoose.model('Notification', NotificationSchema);
