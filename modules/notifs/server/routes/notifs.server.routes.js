cb28a8c222be720070d8da63829db9946b573d32
Ha Nhat Lenh <lenh@ktc.local>
2017-11-28 09:10:25 +0900

    Commited from lenh at Tue, Nov 28, 2017  9:10:25 AM

--- modules/notifs/server/routes/notifs.server.routes.js
+++ modules/notifs/server/routes/notifs.server.routes.js
@@ -7,7 +7,7 @@ var notifsPolicy = require('../policies/notifs.server.policy'),
   notifs = require('../controllers/notifs.server.controller');
 
 module.exports = function(app) {
-  app.route('/api/notifs/count').post(notifs.count);
+  app.route('/api/notifs/count').get(notifs.count);
   // Notifs Routes
   app.route('/api/notifs').all(notifsPolicy.isAllowed)
     .get(notifs.list)
