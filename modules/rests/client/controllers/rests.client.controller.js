d7e85a222c752005dbfd0187104ddb30f2846c14
Ha Nhat Lenh <lenh@ktc.local>
2017-11-28 09:33:54 +0900

    Commited from lenh at Tue, Nov 28, 2017  9:33:54 AM

--- modules/rests/client/controllers/rests.client.controller.js
+++ modules/rests/client/controllers/rests.client.controller.js
@@ -6,9 +6,9 @@
     .module('rests')
     .controller('RestsController', RestsController);
 
-  RestsController.$inject = ['$scope', '$state', 'restResolve', 'CommonService', 'DateUtil', 'RestsApi', 'ngDialog'];
+  RestsController.$inject = ['$scope', '$state', 'restResolve', 'CommonService', 'DateUtil', 'RestsApi', 'ngDialog', 'Socket'];
 
-  function RestsController($scope, $state, rest, CommonService, DateUtil, RestsApi, ngDialog) {
+  function RestsController($scope, $state, rest, CommonService, DateUtil, RestsApi, ngDialog, Socket) {
     var vm = this;
     vm.rest = rest;
 
