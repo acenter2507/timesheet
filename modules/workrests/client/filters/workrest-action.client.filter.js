772359f39897cdd0c5954a39d7d780da885947ad
acenter2507 <acenter2507@gmail.com>
2018-03-23 15:50:00 +0900

    Commited from lenh at Fri, Mar 23, 2018  3:50:00 PM

---
+++ modules/workrests/client/filters/workrest-action.client.filter.js
@@ -0,0 +1,28 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('workrests')
+    .filter('RestActionFilter', RestActionFilter);
+
+  function RestActionFilter() {
+    return function (status) {
+      switch (status) {
+        case 1:
+          return '作成';
+        case 2:
+          return '編集';
+        case 3:
+          return '送信';
+        case 4:
+          return '承認';
+        case 5:
+          return '拒否';
+        case 6:
+          return '使用';
+        default:
+          return '不明';
+      }
+    };
+  }
+}());
---
+++ modules/workrests/client/filters/workrest-status.client.filter.js
@@ -0,0 +1,26 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('workrests')
+    .filter('RestStatusFilter', RestStatusFilter);
+
+  function RestStatusFilter() {
+    return function (status) {
+      switch (status) {
+        case 1:
+          return '未申請';
+        case 2:
+          return '確認中';
+        case 3:
+          return '承認';
+        case 4:
+          return '拒否';
+        case 5:
+          return '終了';
+        default:
+          return '不明';
+      }
+    };
+  }
+}());
