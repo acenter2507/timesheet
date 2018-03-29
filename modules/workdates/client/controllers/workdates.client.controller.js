bd79cc2ac9e3331e35be5ed7abcef034ad57f10b
acenter2507 <acenter2507@gmail.com>
2018-03-29 12:11:44 +0900

    Commited from lenh at Thu, Mar 29, 2018 12:11:44 PM

--- modules/workdates/client/controllers/workdates.client.controller.js
+++ modules/workdates/client/controllers/workdates.client.controller.js
@@ -11,43 +11,39 @@
   function WorkdatesController ($scope, $state, $window, Authentication, workdate) {
     var vm = this;
 
-    vm.authentication = Authentication;
     vm.workdate = workdate;
-    vm.error = null;
-    vm.form = {};
-    vm.remove = remove;
-    vm.save = save;
+    console.log(vm.workdate);
 
-    // Remove existing Workdate
-    function remove() {
-      if ($window.confirm('Are you sure you want to delete?')) {
-        vm.workdate.$remove($state.go('workdates.list'));
-      }
-    }
+    // // Remove existing Workdate
+    // function remove() {
+    //   if ($window.confirm('Are you sure you want to delete?')) {
+    //     vm.workdate.$remove($state.go('workdates.list'));
+    //   }
+    // }
 
-    // Save Workdate
-    function save(isValid) {
-      if (!isValid) {
-        $scope.$broadcast('show-errors-check-validity', 'vm.form.workdateForm');
-        return false;
-      }
+    // // Save Workdate
+    // function save(isValid) {
+    //   if (!isValid) {
+    //     $scope.$broadcast('show-errors-check-validity', 'vm.form.workdateForm');
+    //     return false;
+    //   }
 
-      // TODO: move create/update logic to service
-      if (vm.workdate._id) {
-        vm.workdate.$update(successCallback, errorCallback);
-      } else {
-        vm.workdate.$save(successCallback, errorCallback);
-      }
+    //   // TODO: move create/update logic to service
+    //   if (vm.workdate._id) {
+    //     vm.workdate.$update(successCallback, errorCallback);
+    //   } else {
+    //     vm.workdate.$save(successCallback, errorCallback);
+    //   }
 
-      function successCallback(res) {
-        $state.go('workdates.view', {
-          workdateId: res._id
-        });
-      }
+    //   function successCallback(res) {
+    //     $state.go('workdates.view', {
+    //       workdateId: res._id
+    //     });
+    //   }
 
-      function errorCallback(res) {
-        vm.error = res.data.message;
-      }
-    }
+    //   function errorCallback(res) {
+    //     vm.error = res.data.message;
+    //   }
+    // }
   }
 }());
