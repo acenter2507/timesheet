c9e1d5ae9278077c6bc81a5066afc483f7a0dffc
acenter2507 <acenter2507@gmail.com>
2018-03-29 14:10:51 +0900

    Commited from lenh at Thu, Mar 29, 2018  2:10:51 PM

--- modules/workdates/client/config/workdates.client.routes.js
+++ modules/workdates/client/config/workdates.client.routes.js
@@ -12,59 +12,21 @@
       .state('workdates', {
         abstract: true,
         url: '/workdates',
-        template: '<ui-view/>'
-      })
-      .state('workdates.list', {
-        url: '',
-        templateUrl: 'modules/workdates/client/views/list-workdates.client.view.html',
-        controller: 'WorkdatesListController',
-        controllerAs: 'vm',
-        data: {
-          pageTitle: 'Workdates List'
-        }
-      })
-      .state('workdates.create', {
-        url: '/create',
-        templateUrl: 'modules/workdates/client/views/form-workdate.client.view.html',
-        controller: 'WorkdatesController',
-        controllerAs: 'vm',
-        resolve: {
-          workdateResolve: newWorkdate
-        },
-        data: {
-          roles: ['user', 'admin'],
-          pageTitle: 'Workdates Create'
-        }
-      })
-      .state('workdates.edit', {
-        url: '/:workdateId/edit',
-        templateUrl: 'modules/workdates/client/views/form-workdate.client.view.html',
-        controller: 'WorkdatesController',
-        controllerAs: 'vm',
-        resolve: {
-          workdateResolve: getWorkdate
-        },
-        data: {
-          roles: ['user', 'admin'],
-          pageTitle: 'Edit Workdate {{ workdateResolve.name }}'
-        }
+        template: '<ui-view/>',
+        ncyBreadcrumb: { label: '勤務時間' }
       })
       .state('workdates.view', {
         url: '/:workdateId',
         templateUrl: 'modules/workdates/client/views/view-workdate.client.view.html',
         controller: 'WorkdatesController',
         controllerAs: 'vm',
-        resolve: {
-          workdateResolve: getWorkdate
-        },
-        data: {
-          pageTitle: 'Workdate {{ workdateResolve.name }}'
-        }
+        resolve: { workdateResolve: getWorkdate },
+        data: { roles: ['user'] },
+        ncyBreadcrumb: { label: '入力' }
       });
   }
 
   getWorkdate.$inject = ['$stateParams', 'WorkdatesService'];
-
   function getWorkdate($stateParams, WorkdatesService) {
     return WorkdatesService.get({
       workdateId: $stateParams.workdateId
@@ -72,7 +34,6 @@
   }
 
   newWorkdate.$inject = ['WorkdatesService'];
-
   function newWorkdate(WorkdatesService) {
     return new WorkdatesService();
   }
