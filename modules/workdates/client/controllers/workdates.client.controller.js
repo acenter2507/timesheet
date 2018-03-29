2eaedb61fe85b6a1a6da867fff02bb513a3bcdd5
acenter2507 <acenter2507@gmail.com>
2018-03-29 17:25:46 +0900

    Commited from lenh at Thu, Mar 29, 2018  5:25:46 PM

--- modules/workdates/client/controllers/workdates.client.controller.js
+++ modules/workdates/client/controllers/workdates.client.controller.js
@@ -55,25 +55,30 @@
       // Verify Start
       var isError = false;
       if (vm.workdate.start !== '' && vm.workdate.end !== '' && vm.workdate.content !== '' && vm.workdate.middleRest !== '') {
-
+        console.log('All field inputed');
       } else if (vm.workdate.start === '' && vm.workdate.end === '' && vm.workdate.content === '' && vm.workdate.middleRest === '') {
-
+        console.log('All field empty');
       } else {
         if (vm.workdate.start === '') {
           vm.error.start = { error: true, message: '開始時間を入力してください！' };
           isError = true;
+          console.log('start field empty');
         }
         if (vm.workdate.end === '') {
           vm.error.end = { error: true, message: '終了時間を入力してください！' };
           isError = true;
+          console.log('end field empty');
         }
+        console.log('content: ', vm.workdate.content);
         if (vm.workdate.content === '') {
           vm.error.content = { error: true, message: '作業内容を入力してください！' };
           isError = true;
+          console.log('content field empty');
         }
         if (vm.workdate.middleRest === '') {
           vm.error.middleRest = { error: true, message: '休憩時間を入力してください！' };
           isError = true;
+          console.log('middleRest field empty');
         }
       }
       if (isError) return;
