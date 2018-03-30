ca9c18ec2262442327bea460838d1125ed3ca092
acenter2507 <acenter2507@gmail.com>
2018-03-30 19:11:09 +0900

    Commited from lenh at Fri, Mar 30, 2018  7:11:09 PM

--- modules/workdates/client/controllers/workdates.client.controller.js
+++ modules/workdates/client/controllers/workdates.client.controller.js
@@ -130,7 +130,7 @@
       overtime_duration = NumberUtil.precisionRound(work_duration - rest_duration - Constant.workRange, 1);
       
       console.log("before_overnight_duration: " + before_overnight_duration);
-      console.log("overnight_duration: " + before_overnight_duration);
+      console.log("overnight_duration: " + overnight_duration);
       console.log("rest_duration: " + rest_duration);
       console.log("work_duration: " + work_duration);
       vm.workdate.overtime = overtime_duration;
