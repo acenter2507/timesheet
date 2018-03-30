ae7f298fb3bb112d3344d2a1614c05e000b6708d
acenter2507 <acenter2507@gmail.com>
2018-03-30 19:00:48 +0900

    Commited from lenh at Fri, Mar 30, 2018  7:00:48 PM

--- modules/workdates/client/controllers/workdates.client.controller.js
+++ modules/workdates/client/controllers/workdates.client.controller.js
@@ -127,7 +127,7 @@
 
       // Tính thời gian nghỉ giải lao
       rest_duration = NumberUtil.precisionRound(vm.workdate.middleRest / 60, 1);
-      overtime_duration = NumberUtil.precisionRound(work_duration - rest_duration - Constant.workRange);
+      overtime_duration = NumberUtil.precisionRound(work_duration - rest_duration - Constant.workRange, 1);
       
       vm.workdate.overtime = overtime_duration;
       vm.workdate.overnight = overnight_duration;
