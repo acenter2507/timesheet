7fda0dd29a93cce0dca2f42ae8dc861ab4d8e1ac
acenter2507 <acenter2507@gmail.com>
2018-04-02 14:21:26 +0900

    Commited from lenh at Mon, Apr 02, 2018  2:21:25 PM

--- modules/core/client/services/constant.client.service.js
+++ modules/core/client/services/constant.client.service.js
@@ -6,7 +6,9 @@ Constant.$inject = [];
 function Constant() {
   // Thời gian làm việc trong ngày
   this.workRange = 7.5;
-  // Thời gian tính là khuya
+  // Thời gian bắt đầu tính overnight
   this.overnightStart = '22:00';
+  // Thời gian kết thúc tính overnight
+  this.overnightEnd = '05:00';
   return this;
 }
