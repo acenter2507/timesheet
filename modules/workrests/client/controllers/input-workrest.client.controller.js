7e019633959ec58ea0f0e056194910c32e85e5b9
acenter2507 <acenter2507@gmail.com>
2018-04-05 13:52:57 +0900

    Commited from lenh at Thu, Apr 05, 2018  1:52:57 PM

--- modules/workrests/client/controllers/input-workrest.client.controller.js
+++ modules/workrests/client/controllers/input-workrest.client.controller.js
@@ -111,33 +111,35 @@
       vm.workrest.isPaid = holiday.isPaid;
       vm.workrest.hours = holiday.hours;
 
-      vm.workrest.start = {
-        year: vm.rest_start.year(),
-        month: vm.rest_start.month() + 1,
-        date: vm.rest_start.date()
-      };
-      vm.workrest.end = {
-        year: vm.rest_end.year(),
-        month: vm.rest_end.month() + 1,
-        date: vm.rest_end.date()
-      };
+      console.log(vm.rest_start);
+      console.log(vm.rest_end);
+      // vm.workrest.start = {
+      //   year: vm.rest_start.year(),
+      //   month: vm.rest_start.month() + 1,
+      //   date: vm.rest_start.date()
+      // };
+      // vm.workrest.end = {
+      //   year: vm.rest_end.year(),
+      //   month: vm.rest_end.month() + 1,
+      //   date: vm.rest_end.date()
+      // };
 
-      if (vm.workrest._id) {
-        vm.workrest.$update(successCallback, errorCallback);
-      } else {
-        vm.workrest.$save(successCallback, errorCallback);
-      }
+      // if (vm.workrest._id) {
+      //   vm.workrest.$update(successCallback, errorCallback);
+      // } else {
+      //   vm.workrest.$save(successCallback, errorCallback);
+      // }
 
-      function successCallback(res) {
-        vm.busy = false;
-        Socket.emit('rest_request', { workrestId: res, userId: $scope.user._id });
-        $state.go('workrests.view', { workrestId: res._id });
-      }
+      // function successCallback(res) {
+      //   vm.busy = false;
+      //   Socket.emit('rest_request', { workrestId: res, userId: $scope.user._id });
+      //   $state.go('workrests.view', { workrestId: res._id });
+      // }
 
-      function errorCallback(res) {
-        $scope.handleShowToast(res.data.message, true);
-        vm.busy = false;
-      }
+      // function errorCallback(res) {
+      //   $scope.handleShowToast(res.data.message, true);
+      //   vm.busy = false;
+      // }
     };
     // vm.handleRestDurationChanged = () => {
     //   if (!vm.workrest.start || !vm.workrest.end) {
