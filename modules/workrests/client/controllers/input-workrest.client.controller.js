7abd1252fc4b4d7d413eb7fe577e6333fb631cc6
acenter2507 <acenter2507@gmail.com>
2018-04-06 10:03:24 +0900

    Commited from lenh at Fri, Apr 06, 2018 10:03:24 AM

--- modules/workrests/client/controllers/input-workrest.client.controller.js
+++ modules/workrests/client/controllers/input-workrest.client.controller.js
@@ -125,7 +125,7 @@
 
       vm.workrest.start = moment(vm.rest_start).format('YYYY/MM/DD');
       vm.workrest.end = moment(vm.rest_end).format('YYYY/MM/DD');
-      console.log(vm.workrest)
+      console.log(vm.workrest);
 
       if (vm.workrest._id) {
         vm.workrest.$update(successCallback, errorCallback);
