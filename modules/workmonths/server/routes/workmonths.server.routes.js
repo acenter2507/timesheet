64391095b53e7acaf8f9193d578c586860631c7e
acenter2507 <acenter2507@gmail.com>
2018-04-12 15:46:39 +0900

    Commited from lenh at Thu, Apr 12, 2018  3:46:39 PM

--- modules/workmonths/server/routes/workmonths.server.routes.js
+++ modules/workmonths/server/routes/workmonths.server.routes.js
@@ -6,7 +6,7 @@
 var workmonthsPolicy = require('../policies/workmonths.server.policy'),
   workmonths = require('../controllers/workmonths.server.controller');
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all month in year of 1 user
   app.route('/api/months/workmonthsByYearAndUser').post(workmonths.getMonthsOfYearByUser);
   app.route('/api/months/getHolidayWorking').post(workmonths.getHolidayWorking);
@@ -21,10 +21,10 @@ module.exports = function(app) {
     .put(workmonths.update)
     .delete(workmonths.delete);
 
-    app.route('/api/workmonths/:workmonthId/request').all(workmonthsPolicy.isAllowed).post(workmonths.request);
-    app.route('/api/workmonths/:workmonthId/cancel').all(workmonthsPolicy.isAllowed).post(workmonths.cancel);
-    app.route('/api/workmonths/:workmonthId/approve').all(workmonthsPolicy.isAllowed).post(workmonths.approve);
-    app.route('/api/workmonths/:workmonthId/reject').all(workmonthsPolicy.isAllowed).post(workmonths.reject);
+  app.route('/api/workmonths/:workmonthId/request').all(workmonthsPolicy.isAllowed).post(workmonths.request);
+  app.route('/api/workmonths/:workmonthId/cancel').all(workmonthsPolicy.isAllowed).post(workmonths.cancel);
+  app.route('/api/workmonths/:workmonthId/approve').all(workmonthsPolicy.isAllowed).post(workmonths.approve);
+  app.route('/api/workmonths/:workmonthId/reject').all(workmonthsPolicy.isAllowed).post(workmonths.reject);
 
   // Finish by binding the Workmonth middleware
   app.param('workmonthId', workmonths.workmonthByID);
