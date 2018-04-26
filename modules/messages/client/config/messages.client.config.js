3556cf106459c05235605ff4a93914bc6e7cab26
HNL <acenter2507@gmail.com>
2018-04-26 16:06:26 +0900

    Commited from hanhatlenh at Thu Apr 26 16:06:26 JST 2018

---
+++ modules/messages/client/config/messages.client.config.js
@@ -0,0 +1,32 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('messages')
+    .run(menuConfig);
+
+  menuConfig.$inject = ['menuService'];
+
+  function menuConfig(menuService) {
+    // Set top bar menu items
+    menuService.addMenuItem('topbar', {
+      title: 'Messages',
+      state: 'messages',
+      type: 'dropdown',
+      roles: ['*']
+    });
+
+    // Add the dropdown list item
+    menuService.addSubMenuItem('topbar', 'messages', {
+      title: 'List Messages',
+      state: 'messages.list'
+    });
+
+    // Add the dropdown create item
+    menuService.addSubMenuItem('topbar', 'messages', {
+      title: 'Create Message',
+      state: 'messages.create',
+      roles: ['user']
+    });
+  }
+}());
