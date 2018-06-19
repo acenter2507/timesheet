bf2dacdc61425b278186905625b27d7f0961a8b9
acenter2507 <acenter2507@gmail.com>
2018-06-19 16:53:13 +0900

    Commited from lenh at Tue, Jun 19, 2018  4:53:12 PM

--- modules/messages/client/controllers/list-messages.client.controller.js
+++ modules/messages/client/controllers/list-messages.client.controller.js
@@ -5,11 +5,32 @@
     .module('messages')
     .controller('MessagesListController', MessagesListController);
 
-  MessagesListController.$inject = ['MessagesService'];
+  MessagesListController.$inject = ['$state', '$scope', 'MessagesService', 'Messages'];
 
-  function MessagesListController(MessagesService) {
+  function MessagesListController($state, $scope, MessagesService, Messages) {
     var vm = this;
 
-    vm.messages = MessagesService.query();
+    onCreate();
+    function onCreate() {
+      prepareMessages();
+    }
+    function prepareMessages() {
+      MessagesService.query().$promise.then(function (messages) {
+        vm.messages = messages;
+      });
+    }
+    vm.handleClearAllMessages = function () {
+      $scope.handleShowConfirm({
+        message: '全ての通知を削除しますか？'
+      }, function () {
+        Messages.clear().then(function () {
+          vm.messages = [];
+        });
+      });
+    };
+    vm.handleRemoveMessage = function (message) {
+      Messages.remove(message._id);
+      vm.messages = _.without(vm.messages, message);
+    };
   }
 }());
