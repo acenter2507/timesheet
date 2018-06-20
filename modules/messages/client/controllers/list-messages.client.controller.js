45616959c11d99b3ce55d617dc8f7758498a81a0
acenter2507 <acenter2507@gmail.com>
2018-06-20 11:33:34 +0900

    Commited from lenh at Wed, Jun 20, 2018 11:33:34 AM

--- modules/messages/client/controllers/list-messages.client.controller.js
+++ modules/messages/client/controllers/list-messages.client.controller.js
@@ -35,7 +35,7 @@
           vm.messages = _.union(vm.messages, _messages);
           vm.page += 1;
           vm.busy = false;
-          if (_messages.length < 10) vm.stopped = true;
+          if (_messages.length < 20) vm.stopped = true;
         }
         if (!$scope.$$phase) $scope.$digest();
       })
