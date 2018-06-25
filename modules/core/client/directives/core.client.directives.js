efc5ccdc639f56d42e42ddf6c78ae68dfb8a79c9
acenter2507 <acenter2507@gmail.com>
2018-06-25 16:58:18 +0900

    Commited from lenh at Mon, Jun 25, 2018  4:58:18 PM

--- modules/core/client/directives/core.client.directives.js
+++ modules/core/client/directives/core.client.directives.js
@@ -142,10 +142,10 @@ function convertToNumber() {
     require: 'ngModel',
     link: function (scope, element, attrs, ngModel) {
       ngModel.$parsers.push(function (val) {
-        return val != null ? parseInt(val, 10) : null;
+        return val !== null ? parseInt(val, 10) : null;
       });
       ngModel.$formatters.push(function (val) {
-        return val != null ? '' + val : null;
+        return val !== null ? '' + val : null;
       });
     }
   };
