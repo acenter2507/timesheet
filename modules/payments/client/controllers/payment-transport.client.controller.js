dedd98d0f6ebb0ff2f81918132086f64dd0fa033
acenter2507 <acenter2507@gmail.com>
2018-06-25 17:34:56 +0900

    Commited from lenh at Mon, Jun 25, 2018  5:34:56 PM

--- modules/payments/client/controllers/payment-transport.client.controller.js
+++ modules/payments/client/controllers/payment-transport.client.controller.js
@@ -9,7 +9,32 @@
     'FileUploader'
   ];
 
+
   function PaymentTransportController($scope, FileUploader) {
-    console.log($scope.transport);
+
+    prepareUpload();
+
+    function prepareUpload() {
+      $scope.uploader = new FileUploader({
+        url: 'api/payments/images',
+        alias: 'imagesUpload'
+      });
+      $scope.uploader.filters.push({
+        name: 'imageFilter',
+        fn: function (item, options) {
+          var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+          return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+        }
+      });
+      $scope.uploader.onAfterAddingAll = function (addedFileItems) {
+        console.info('onAfterAddingAll', addedFileItems);
+      };
+      $scope.uploader.onCompleteItem = function (fileItem, response, status, headers) {
+        $scope.results.push(response);
+      };
+      $scope.uploader.onCompleteAll = function () {
+        $scope.closeThisDialog($scope.results);
+      };
+    }
   }
 })();
