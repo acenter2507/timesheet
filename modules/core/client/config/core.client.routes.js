4d7fc39f61d4a48b861833e54cf21c19ca15d948
acenter2507 <acenter2507@gmail.com>
2018-06-29 14:14:21 +0900

    Commited from lenh at Fri, Jun 29, 2018  2:14:21 PM

--- modules/core/client/config/core.client.routes.js
+++ modules/core/client/config/core.client.routes.js
@@ -17,6 +17,8 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
         url: '/',
         templateUrl: 'modules/core/client/views/home.client.view.html',
         data: { roles: ['user'] },
+        controller: 'HomeController',
+        controllerAs: 'vm',
         ncyBreadcrumb: { label: 'ホーム' }
       })
       .state('not-found', {
