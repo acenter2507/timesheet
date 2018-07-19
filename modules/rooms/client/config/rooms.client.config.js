bd8ded0440e6ccbbabbbee701ed8993c55a1a6f6
HNL <acenter2507@gmail.com>
2018-07-19 19:20:56 +0900

    Commited from hanhatlenh at Thu Jul 19 19:20:56 JST 2018

---
+++ modules/rooms/client/config/rooms.client.config.js
@@ -0,0 +1,32 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('rooms')
+    .run(menuConfig);
+
+  menuConfig.$inject = ['menuService'];
+
+  function menuConfig(menuService) {
+    // Set top bar menu items
+    menuService.addMenuItem('topbar', {
+      title: 'Rooms',
+      state: 'rooms',
+      type: 'dropdown',
+      roles: ['*']
+    });
+
+    // Add the dropdown list item
+    menuService.addSubMenuItem('topbar', 'rooms', {
+      title: 'List Rooms',
+      state: 'rooms.list'
+    });
+
+    // Add the dropdown create item
+    menuService.addSubMenuItem('topbar', 'rooms', {
+      title: 'Create Room',
+      state: 'rooms.create',
+      roles: ['user']
+    });
+  }
+}());
