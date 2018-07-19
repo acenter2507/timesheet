4ed52a6670e14c9a2932c971717906352fa3e148
acenter2507 <acenter2507@gmail.com>
2018-07-19 11:28:02 +0900

    Commited from lenh at Thu, Jul 19, 2018 11:28:02 AM

--- modules/workmonths/client/controllers/workmonths.client.controller.js
+++ modules/workmonths/client/controllers/workmonths.client.controller.js
@@ -56,8 +56,7 @@
       }, function () {
         WorkmonthsApi.request(vm.workmonth._id)
           .success(function (workmonth) {
-            console.log(workmonth);
-            _.extend(vm.workmonth, workmonth);
+            _.extendOwn(vm.workmonth, workmonth);
             Socket.emit('month_request', { workmonthId: vm.workmonth._id, userId: $scope.user._id });
           })
           .error(function (err) {
