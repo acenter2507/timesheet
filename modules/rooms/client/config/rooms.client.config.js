555c6cc6290e472c2cb1830500629b8ba5a878f1
acenter2507 <acenter2507@gmail.com>
2018-07-19 19:32:50 +0900

    Commited from lenh at Thu, Jul 19, 2018  7:32:49 PM

--- modules/rooms/client/config/rooms.client.config.js
+++ modules/rooms/client/config/rooms.client.config.js
@@ -5,7 +5,7 @@
     .module('rooms')
     .run(menuConfig);
 
-  menuConfig.$inject = ['menuService'];
+  menuConfig.$inject = ['Menus'];
 
   function menuConfig(menuService) {
     // Set top bar menu items
