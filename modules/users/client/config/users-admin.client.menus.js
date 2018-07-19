44e9dc5db885b28db6a00883f75d7448e3c2fbce
acenter2507 <acenter2507@gmail.com>
2018-07-19 16:01:35 +0900

    Commited from lenh at Thu, Jul 19, 2018  4:01:35 PM

--- modules/users/client/config/users-admin.client.menus.js
+++ modules/users/client/config/users-admin.client.menus.js
@@ -3,12 +3,12 @@
 // Configuring the Articles module
 angular.module('users.admin').run(['Menus',
   function (Menus) {
-    Menus.addMenuItem('topbar', {
-      title: '社員',
-      state: 'users.list',
-      // type: 'dropdown',
-      roles: ['admin', 'accountant', 'manager']
-    });
+    // Menus.addMenuItem('topbar', {
+    //   title: '社員',
+    //   state: 'users.list',
+    //   // type: 'dropdown',
+    //   roles: ['admin', 'accountant', 'manager']
+    // });
 
     // Menus.addSubMenuItem('topbar', 'users', {
     //   title: 'List Users',
