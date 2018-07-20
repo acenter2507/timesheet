9a463e4ff38c64c740ef34ea9ab4c4da212efd7f
acenter2507 <acenter2507@gmail.com>
2018-07-20 11:16:21 +0900

    Commited from lenh at Fri, Jul 20, 2018 11:16:21 AM

--- modules/rooms/server/models/room.server.model.js
+++ modules/rooms/server/models/room.server.model.js
@@ -10,7 +10,18 @@ var mongoose = require('mongoose'),
 var RoomSchema = new Schema({
   name: { type: String, required: '会議室の名前は必須です。', trim: true },
   info: { type: String, default: '' },
+  // 席数
   seats: { type: Number, default: 1 },
+  // プロジェクター
+  projector: { type: Boolean, default: false },
+  // エアコン
+  air_conditional: { type: Boolean, default: false },
+  // ホワイトボードー
+  white_board: { type: Boolean, default: false },
+  // パソコン
+  computer: { type: Number, default: 0 },
+  // 音響
+  sound: { type: Boolean, default: false },
   created: { type: Date, default: Date.now },
   user: { type: Schema.ObjectId, ref: 'User' }
 });
