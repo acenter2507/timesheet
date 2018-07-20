7090a601549e9c55ef4314f1b179b929f627a222
acenter2507 <acenter2507@gmail.com>
2018-07-20 17:56:09 +0900

    Commited from lenh at Fri, Jul 20, 2018  5:56:09 PM

--- modules/rooms/client/controllers/bookings.client.controller.js
+++ modules/rooms/client/controllers/bookings.client.controller.js
@@ -12,13 +12,18 @@
     var vm = this;
     vm.booking = booking;
     vm.step = 1;
+    vm.current = moment();
 
     preapareCondition();
     function preapareCondition() {
       vm.condition = {
-        min_date: new Date(),
-        start_date: new Date(),
-        end_date: new Date()
+        min_date: vm.current.format('YYYY/MM/DD'),
+        start_date: vm.current.format('YYYY/MM/DD'),
+        end_date: vm.current.format('YYYY/MM/DD'),
+        start_hour: '08:00',
+        end_hour: '23:30',
+        // start_hour: vm.current.startOf('hour').add(1, 'hours').format('HH:mm'),
+        // end_hour: vm.current.startOf('hour').add(2, 'hours').format('HH:mm')
       };
     }
 
