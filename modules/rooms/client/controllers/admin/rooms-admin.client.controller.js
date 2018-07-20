d32176f5a8d3ef43d2a4464b179c70bfaf4b74c1
acenter2507 <acenter2507@gmail.com>
2018-07-20 11:23:09 +0900

    Commited from lenh at Fri, Jul 20, 2018 11:23:09 AM

--- modules/rooms/client/controllers/admin/rooms-admin.client.controller.js
+++ modules/rooms/client/controllers/admin/rooms-admin.client.controller.js
@@ -5,10 +5,23 @@
     .module('rooms.admin')
     .controller('RoomsAdminController', RoomsAdminController);
 
-  RoomsAdminController.$inject = ['RoomsService'];
+  RoomsAdminController.$inject = ['RoomsService', '$state'];
 
-  function RoomsAdminController(RoomsService) {
+  function RoomsAdminController(RoomsService, $state) {
     var vm = this;
     vm.rooms = RoomsService.query();
+
+    vm.hanleSelectRoom = function (room) {
+      $state.go('admin.rooms.view', { roomId: room._id });
+    };
+    vm.handleDeleteRoom = function (room) {
+      $scope.handleShowConfirm({
+        message: room.name + 'を削除しますか？'
+      }, function () {
+        room.$remove(function () {
+          vm.rooms = _.without(vm.rooms, room);
+        });
+      });
+    };
   }
 }());
