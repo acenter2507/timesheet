ee9f8d9af11286e157897e3ee59709360423f31f
acenter2507 <acenter2507@gmail.com>
2018-07-20 16:56:12 +0900

    Commited from lenh at Fri, Jul 20, 2018  4:56:12 PM

--- modules/rooms/client/controllers/bookings.client.controller.js
+++ modules/rooms/client/controllers/bookings.client.controller.js
@@ -13,6 +13,21 @@
     vm.booking = booking;
     vm.step = 1;
 
+    vm.handleNextToRooms = function() {
+      vm.step = 2;
+    };
+    vm.handleNextToConfirm = function() {
+      vm.step = 3;
+    };
+    vm.handleSaveBooking = function() {
+      alert('Save');
+    };
+    vm.handleBackToCondition = function() {
+      vm.step = 1;
+    };
+    vm.handleBackToRooms = function() {
+      vm.step = 2;
+    };
     function validateCondition() {
 
     }
