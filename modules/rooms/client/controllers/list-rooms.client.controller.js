cea4171c101c3ead670379b0678a1fb6ddbbf319
acenter2507 <acenter2507@gmail.com>
2018-07-23 17:41:55 +0900

    Commited from lenh at Mon, Jul 23, 2018  5:41:55 PM

--- modules/rooms/client/controllers/list-rooms.client.controller.js
+++ modules/rooms/client/controllers/list-rooms.client.controller.js
@@ -9,7 +9,25 @@
 
   function RoomsListController(RoomsService) {
     var vm = this;
-
     vm.rooms = RoomsService.query();
+
+    onCreate();
+    function onCreate() {
+      prepareCalendar();
+    }
+    function prepareCalendar() {
+      vm.calendar = { view: 'day' };
+      vm.calendar.viewDate = moment().startOf('day').toDate();
+      vm.handleCalendarEventClicked = function () {
+        return false;
+      };
+      vm.handleCalendarRangeSelected = function (start, end) {
+        return false;
+      };
+      vm.handleCalendarClicked = function (date) {
+        return false;
+      };
+    }
+    
   }
 }());
