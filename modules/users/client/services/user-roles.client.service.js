e0429d4eb3206814d0d10c4c014433ee6786cf09
acenter2507 <acenter2507@gmail.com>
2018-07-24 17:14:15 +0900

    Commited from lenh at Tue, Jul 24, 2018  5:14:14 PM

--- modules/users/client/services/user-roles.client.service.js
+++ modules/users/client/services/user-roles.client.service.js
@@ -8,10 +8,7 @@
   function UserRolesService() {
     this.roles = [
       { name: '管理者', value: 'admin' },
-      { name: '11111', value: '22222' },
       { name: 'マネージャ', value: 'manager' },
-      { name: '経理', value: 'accountant' },
-      { name: '経理2', value: 'accountant1' },
       { name: '一般社員', value: 'user' },
       { name: '確認者', value: 'reviewer' },
       { name: '参照者', value: 'viewer' }
