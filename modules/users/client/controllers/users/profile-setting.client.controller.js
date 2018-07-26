d890e334aa53b3df3ee6625344f3b2e4253885b8
acenter2507 <acenter2507@gmail.com>
2018-07-26 15:17:49 +0900

    Commited from lenh at Thu, Jul 26, 2018  3:17:49 PM

--- modules/users/client/controllers/users/profile-setting.client.controller.js
+++ modules/users/client/controllers/users/profile-setting.client.controller.js
@@ -7,6 +7,9 @@ angular
 ProfileSettingController.$inject = ['$scope', '$state'];
 
 function ProfileSettingController($scope, $state) {
+
+  $scope.busy = false;
+
   onCreate();
   function onCreate() {
     if ($scope.user.birthdate) {
@@ -14,4 +17,19 @@ function ProfileSettingController($scope, $state) {
     }
 
   }
+
+  $scope.changeUserPassword = function (isValid) {
+    if (!isValid) {
+      $scope.$broadcast('show-errors-check-validity', 'passwordForm');
+      return false;
+    }
+
+    $http.post('/api/users/password', $scope.passwordDetails).success(function (response) {
+      $scope.$broadcast('show-errors-reset', 'passwordForm');
+      $scope.handleShowToast('パスワードを変更しました。', false);
+      $scope.passwordDetails = null;
+    }).error(function (err) {
+      $scope.handleShowToast(err.message, true);
+    });
+  };
 }
