63d40ab39bdb0102c22d41fd8998fdb07140c197
acenter2507 <acenter2507@gmail.com>
2018-07-26 14:46:15 +0900

    Commited from lenh at Thu, Jul 26, 2018  2:46:15 PM

--- modules/users/client/controllers/users/profile-setting.client.controller.js
+++ modules/users/client/controllers/users/profile-setting.client.controller.js
@@ -7,9 +7,18 @@ angular
 ProfileSettingController.$inject = ['$scope', '$state', '$stateParams'];
 
 function ProfileSettingController($scope, $state, $stateParams) {
-  $scope.action = $stateParams.action || 1;
-
-  $scope.isActive = function (number) {
-    return $scope.action === number;
-  };
+  $scope.tabs = [
+    { title: '個人情報', active: true },
+    { title: 'パスワード変更', active: false }
+  ];
+  onCreate();
+  function onCreate() {
+    if ($stateParams.action) {
+      if ($stateParams.action === 1) {
+        $scope.tabs[0].active = true;
+      } else if ($stateParams.action === 2) {
+        $scope.tabs[1].active = true;
+      }
+    }
+  }
 }
