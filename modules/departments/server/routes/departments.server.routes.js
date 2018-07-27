9e011f64d0398d6e5125f150c614e48d3681e2b5
acenter2507 <acenter2507@gmail.com>
2018-07-27 15:14:29 +0900

    Commited from lenh at Fri, Jul 27, 2018  3:14:29 PM

--- modules/departments/server/routes/departments.server.routes.js
+++ modules/departments/server/routes/departments.server.routes.js
@@ -8,15 +8,11 @@ var departmentsPolicy = require('../policies/departments.server.policy'),
   manager = require('../controllers/departments-manager.server.controller');
 
 module.exports = function (app) {
-  app.route('/api/departments/autocomplete').all(departmentsPolicy.isAllowed).post(departments.autocomplete);
-  app.route('/api/departments').all(departmentsPolicy.isAllowed).get(departments.list);
-  app.route('/api/departments/:departmentId').all(departmentsPolicy.isAllowed).get(departments.read);
-
   // マネージャー
-  app.route('/api/departments/manager').all(departmentsPolicy.isAllowed)
-    .post(manager.create)
-    .get(manager.list);
   app.route('/api/departments/manager/avatar').post(departmentsPolicy.isAllowed, manager.avatar);
+  app.route('/api/departments/manager').all(departmentsPolicy.isAllowed)
+    .get(manager.list)
+    .post(manager.create);
 
   app.route('/api/departments/manager/:departmentId').all(departmentsPolicy.isAllowed)
     .get(manager.read)
@@ -26,5 +22,8 @@ module.exports = function (app) {
     .post(manager.removeUser)
     .put(manager.addUser);
 
+  app.route('/api/departments/autocomplete').all(departmentsPolicy.isAllowed).post(departments.autocomplete);
+  app.route('/api/departments').all(departmentsPolicy.isAllowed).get(departments.list);
+  app.route('/api/departments/:departmentId').all(departmentsPolicy.isAllowed).get(departments.read);
   app.param('departmentId', departments.departmentByID);
 };
